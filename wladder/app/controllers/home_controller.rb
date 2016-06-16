72fb9655bfe49a1d4eb776fa83a2f85ae5ca7ff7
Ubuntu <ubuntu@ip-172-31-34-253.us-west-2.compute.internal>
2016-06-16 01:14:50 +0000

    Moving to Local Directory...

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 
+# This file is ruby. It adds variables for your views to use
+
 load "#{Rails.root}/lib/ladder.rb"
 
   def index
@@ -7,12 +9,30 @@ load "#{Rails.root}/lib/ladder.rb"
   end
 
   def show
-    @query_text = params[:query]
-    if @query_text =~ /^[a-z]*$/
-      @result = `whois #{@query_text}`.split("\n")
+    iter = 0
+    @query_text = Array.new
+    @query_text << params[:query1]
+    @query_text << params[:query2]
+    @query_text << params[:query3]
+    @query_text << params[:query4]
+    @query_text << params[:query5]
+
+    if @query_text[iter] =~ /^[a-z]*$/
+	if edit_distance_of_one(@query_text[iter], @query_text[iter + 1])
+		if legal(@query_text[iter])
+			# Everything true/legal
+			iter += 1
+      			@result = ["You Win Mofo!"]
+		else
+			@result = ["Illegal Word"];
+		end
+	else
+		@result = ["Illegal Word"];
+	end
     else
-      @result = ["Invalid Input"];
+      @result = ["Illegal Word"];
     end
+
   end
 
 end
