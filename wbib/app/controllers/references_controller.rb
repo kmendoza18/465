7c8f75343f4c8545b8c311c3553d792a99a816e0
Ubuntu <ubuntu@ip-172-31-34-253.us-west-2.compute.internal>
2016-06-20 11:41:26 -0700

    Working. Edit Implemented

--- wbib/app/controllers/references_controller.rb
+++ wbib/app/controllers/references_controller.rb
@@ -1,7 +1,43 @@
 class ReferencesController < ApplicationController
 
-	# Get /references
+	# GET /references/new
+	def new
+		@reference = Reference.new
+	end
+
+	# Get /references/1/edit
+	def edit
+		@reference = Reference.find(params[:id])
+	end
+
+	# GET /references
 	def index
 		@references = Reference.all
+	end
+
+	# GET /references/1
+	def show
+		@reference = Reference.find(params[:id])
+	end
+
+	# POST /references
+	def create
+		@reference = Reference.new(params.require(:reference).permit(:url, :topic, :annotation, :timedate))
+		
+		if @reference.save
+			redirect_to @reference, notice: 'Reference Successfully Created'
+		else
+			render :new
+		end
+	end
+
+	# PATCH /references/1
+	def update
+		@reference = Reference.find(params[:id])
+		if @reference.update(params.require(:reference).permit(:url, :topic, :annotation, :timedate))
+			redirect_to @reference, notice: 'Reference Successfully Updated'
+		else
+			render :edit
+		end
 	end
 end
--- wbib/config/routes.rb
+++ wbib/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+	# Order Matters
+	get 'references/new', to: 'references#new', as: 'new_reference'
+	get 'references/:id/edit', to: 'references#edit', as: 'edit_reference'
+	get 'references/:id', to: 'references#show', as: 'reference'
+	get 'references', to: 'references#index', as: 'references'
+	post 'references', to: 'references#create'
+	patch 'references/:id', to: 'references#update'
+	
 	root 'references#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
