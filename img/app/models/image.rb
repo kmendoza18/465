d5c63834d402ff4060b29cf0e6139063824f62fd
Ubuntu <ubuntu@ip-172-31-34-253.us-west-2.compute.internal>
2016-06-28 10:53:17 -0700

    Delete Fixed

--- img/app/models/image.rb
+++ img/app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-    has_many :user, through: :imageuser, dependent: :destroy
-	has_many :imageuser, dependent: :destroy
+    has_many :user, through: :image_user, dependent: :destroy
+	has_many :image_user, dependent: :destroy
 	has_many :tag
 end
