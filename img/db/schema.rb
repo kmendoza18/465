83474ccbc63673b097d55922c1fcf866fecb5843
Ubuntu <ubuntu@ip-172-31-34-253.us-west-2.compute.internal>
2016-06-28 14:15:22 -0700

    Things still broken

--- img/db/schema.rb
+++ img/db/schema.rb
@@ -14,33 +14,40 @@
 ActiveRecord::Schema.define(version: 20160627184757) do
 
   create_table "image_users", force: :cascade do |t|
-    t.string   "image_id"
-    t.string   "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "image_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "image_users", ["image_id"], name: "index_image_users_on_image_id"
+  add_index "image_users", ["user_id"], name: "index_image_users_on_user_id"
 
   create_table "images", force: :cascade do |t|
     t.string   "filename"
     t.boolean  "status"
-    t.string   "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "images", ["user_id"], name: "index_images_on_user_id"
 
   create_table "tags", force: :cascade do |t|
     t.string   "str"
-    t.string   "image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "image_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "tags", ["image_id"], name: "index_tags_on_image_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password"
     t.string   "name"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
