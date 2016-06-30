3cd27acbd0b96417ed0a2c8b634f5c073f95bb86
Ubuntu <ubuntu@ip-172-31-34-253.us-west-2.compute.internal>
2016-06-30 13:34:05 -0700

    A few more things impemented. 90% done

--- img/app/controllers/tags_controller.rb
+++ img/app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+    before_action :set_tag, only: [:show, :edit, :update, :destroy]
 
     def index
         @image = Image.find params[:image_id]
@@ -50,7 +51,11 @@ class TagsController < ApplicationController
 
   private
 
+    def set_tag
+        @tag = Tag.find(params[:id])
+    end
+
     def tag_params
-      params.require(:tag).permit(:str)
+      params.require(:tag).permit(:str, :image_id)
     end
 end
